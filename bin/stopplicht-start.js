38c34713e7025c9527f83c058093b0f5ef093fc4
Dusty Burwell <dburwell@groupon.com>
2014-10-29 21:17:44 -0500

    Switch to node-webkit to fix yosemite issues. Also gains cross platform, woot!

--- bin/stopplicht-start.js
+++ bin/stopplicht-start.js
@@ -3,7 +3,7 @@
 var fs = require('fs');
 
 var spawn = require('child_process').spawn;
-var script = __dirname + '/../lib/index.js';
-var child = spawn('node', [script], { detached: true , stdio: [ 'ignore', 'ignore', 'ignore' ] });
+var script = __dirname + '/../node_modules/.bin/nodewebkit';
+var child = spawn(script, [], { detached: true , stdio: [ 'ignore', 'ignore', 'ignore' ] });
 
 child.unref();
