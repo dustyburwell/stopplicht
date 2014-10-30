38e560304ed2f409efc4213e1aff484fb8110c2d
Dusty Burwell <dburwell@groupon.com>
2014-10-29 22:02:15 -0500

    Fix an issue with working directory

--- bin/stopplicht-start.js
+++ bin/stopplicht-start.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var path = require('path');
 
+process.chdir(path.join(__dirname, '/../'))
 var spawn = require('child_process').spawn;
-var script = __dirname + '/../node_modules/.bin/nodewebkit';
+var script = path.join(__dirname, '/../node_modules/.bin/nodewebkit');
 var child = spawn(script, [], { detached: true , stdio: [ 'ignore', 'ignore', 'ignore' ] });
 
 child.unref();
