96d6d2840134ba3f36746c9250cde52e46e57bcd
Dusty Burwell <dburwell@groupon.com>
2014-10-29 23:28:18 -0500

    Make adjustments for stopplicht to work on Windows

--- bin/stopplicht-start.js
+++ bin/stopplicht-start.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
-
 var fs = require('fs');
 var path = require('path');
-
-process.chdir(path.join(__dirname, '/../'))
+var cwd = path.join(__dirname, '..');
 var spawn = require('child_process').spawn;
-var script = path.join(__dirname, '/../node_modules/.bin/nodewebkit');
-var child = spawn(script, [], { detached: true , stdio: [ 'ignore', 'ignore', 'ignore' ] });
+var script = path.join(__dirname, '..', 'node_modules', 'nodewebkit', 'bin', 'nodewebkit');
+var child = spawn('node', [script], {cwd: cwd}, { detached: true , stdio: 'ignore' });
 
 child.unref();
+process.exit();
