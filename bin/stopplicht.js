68ca7ca299007f35ba7aeaaf29f85de624e56d0e
Dusty Burwell <dburwell@groupon.com>
2014-11-01 00:06:13 -0500

    Allow notification messages to be sent via stopplicht command

--- bin/stopplicht.js
+++ bin/stopplicht.js
@@ -3,9 +3,11 @@
 var cp = require('child_process')
 var args = process.argv.slice(2);
 
-var command = args[0] || 'start';
+if (!args.length) {
+  args.unshift('start');
+}
 
-cp.exec('stopplicht-' + command, function(err, stdout, stderr) {
+cp.exec('stopplicht-' + args.join(' '), function(err, stdout, stderr) {
   process.stdout.write(stdout);
   process.stderr.write(stderr);
 });
