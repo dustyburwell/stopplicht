8af45485647d6a870080f872bf512b838639a1cd
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2014-11-07 00:49:37 -0600

    Fix options

--- bin/stopplicht-start.js
+++ bin/stopplicht-start.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var path = require('path');
 var cwd = path.join(__dirname, '..');
 var script = path.join(__dirname, '..', 'node_modules', 'nodewebkit', 'bin', 'nodewebkit');
-var child = cp.spawn('node', [script], {cwd: cwd}, { detached: true , stdio: 'ignore' });
+var child = cp.spawn('node', [script], {cwd: cwd, detached: true , stdio: 'ignore' });
 
 child.unref();
 process.exit();
