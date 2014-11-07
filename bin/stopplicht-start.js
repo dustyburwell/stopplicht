2cbe05b973894bea1ff7c8481e62865ccffcd2c7
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2014-11-07 00:49:37 -0600

    Fix options Conflicts: 	bin/stopplicht-start.js

--- bin/stopplicht-start.js
+++ bin/stopplicht-start.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cwd = path.join(__dirname, '..');
 var spawn = require('child_process').spawn;
 var script = path.join(__dirname, '..', 'node_modules', 'nodewebkit', 'bin', 'nodewebkit');
-var child = spawn('node', [script], {cwd: cwd}, { detached: true , stdio: 'ignore' });
+var child = spawn('node', [script], {cwd: cwd, detached: true , stdio: 'ignore' });
 
 child.unref();
 process.exit();
